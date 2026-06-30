283312ee33fd0f3eebbeefe1c4b0205eb6dedfc1
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2026-06-29 21:59:40 -0600

    feat: add AMQP connection setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const connString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ.")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down.")
 }
